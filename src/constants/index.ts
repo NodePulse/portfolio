28b4d05fc8aa39b664d3f1a75582c6bb112b07d7
NodePulse <bharbeysachin@gmail.com>
2025-05-05 11:04:06 +0530

    Hero section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -46,3 +46,10 @@ export const words: WordsProps[] = [
     imgPath: "/images/code.svg",
   },
 ];
+
+export const counterItems = [
+  { value: 15, suffix: "+", label: "Years of Experience" },
+  { value: 200, suffix: "+", label: "Satisfied Clients" },
+  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
